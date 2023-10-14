e8b2328831ffaf5b8c5d9e4e254da12e8b4f414e
s3nmith <lahiruudawatta@gmail.com>
2023-10-14 21:58:18 +0900

    api test2

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -5,13 +5,17 @@ type Data = {
   receivedData: string | null;
 };
 
+let storedData: string | null = null; // In-memory storage for the data
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   if (req.method === "POST") {
-    const receivedData = req.body.data || null;
-    res.status(200).json({ receivedData });
+    storedData = req.body.data || null;
+    res.status(200).json({ receivedData: storedData });
+  } else if (req.method === "GET") {
+    res.status(200).json({ receivedData: storedData });
   } else {
     res.status(405).json({ receivedData: "Method not allowed" });
   }

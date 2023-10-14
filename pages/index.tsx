683af0be20198651e01e29e1850ba441b1f2865e
s3nmith <lahiruudawatta@gmail.com>
2023-10-14 16:29:00 +0900

    UI improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 interface CampusTileProps {
   title: string;
+  link: string;
 }
 
-const CampusTile: React.FC<CampusTileProps> = ({ title }) => {
+const CampusTile: React.FC<CampusTileProps> = ({ title, link }) => {
   return (
-    <div className="cursor-pointer border p-5 m-4 hover:bg-gray-100 transition duration-300 ease-in-out rounded shadow-lg">
-      <h3 className="text-xl font-semibold">{title}</h3>
-    </div>
+    <Link href={link}>
+      <div className="tile">
+        <h3>{title}</h3>
+      </div>
+    </Link>
   );
 };
 
@@ -20,21 +24,24 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="flex items-center">
+      <div className="header">
         <Image
           src="/wasedaLine1.png"
           alt="WasedaLine Logo"
-          width={100}
-          height={100}
+          width={200}
+          height={200}
         />
-        <h1 className="ml-4">WasedaLine</h1>
+        <h1 className="title"></h1>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      <div className="tile-layout">
         <div className="camp-tiles">
-          <CampusTile title="Main Campus/早稲田キャンパス" />
-          <CampusTile title="NishiWaseda Campus/西早稲田キャンパス" />
-          <CampusTile title="Toyama Campus/戸山キャンパス" />
+          <CampusTile title="Main Campus/早稲田キャンパス" link="/honcamp" />
+          <CampusTile
+            title="NishiWaseda Campus/西早稲田キャンパス"
+            link="/nishiwaseda"
+          />
+          <CampusTile title="Toyama Campus/戸山キャンパス" link="/toyama" />
         </div>
       </div>
     </main>
---
+++ pages/nishiwaseda/index.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+// replace this with actual data from an API
+const mockData = [
+  { name: "Building 63", queue: 20 },
+  { name: "Building 51", queue: 5 },
+];
+
+export default function NishiWaseda() {
+  const [cafeterias, setCafeterias] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from an API or use mock data
+    setCafeterias(mockData);
+  }, []);
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">NishiWaseda</h1>
+      <div className="back-butt">
+        <Link href="/">
+          <h2>back</h2>
+        </Link>
+      </div>
+
+      <div className="grid gap-4">
+        {cafeterias.map((cafeteria, index) => (
+          <div key={index} className="p-4 border rounded shadow-lg">
+            <h2 className="text-xl font-semibold">{cafeteria.name}</h2>
+            <p className="mt-2">
+              Current Queue:{" "}
+              <span className="font-bold">{cafeteria.queue}</span> people
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
